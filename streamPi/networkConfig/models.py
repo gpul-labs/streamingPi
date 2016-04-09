bb34175eb4dead9d8be32297d81c417e0ee749ca
José Millán Soto <fid@gpul.org>
2016-04-09 17:33:53 +0200

    Generate Network Manager configuration files when saving a network profile

--- streamPi/networkConfig/models.py
+++ streamPi/networkConfig/models.py
@@ -15,16 +15,73 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.conf import settings
 from django.db import models
 
+import os
+import string
+import uuid
+
 class Network(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
+    uuid = models.CharField(max_length=36, blank=True)
     use_dhcp = models.BooleanField()
-    ip4_address = models.CharField(max_length=16)
-    ip4_netmask = models.CharField(max_length=16)
-    ip4_gateway = models.CharField(max_length=16)
-    ip4_dns1 = models.CharField(max_length=16)
-    ip4_dns2 = models.CharField(max_length=16)
+    ip4_address = models.CharField(max_length=16, blank=True)
+    ip4_netmask = models.CharField(max_length=16, blank=True)
+    ip4_gateway = models.CharField(max_length=16, blank=True)
+    ip4_dns1 = models.CharField(max_length=16, blank=True)
+    ip4_dns2 = models.CharField(max_length=16, blank=True)
+
+    fileName = models.CharField(max_length=256, blank=True)
+
+    def generateConfig(self):
+        config = {}
+        config['connection'] = {}
+        config['connection']['type'] = 'ethernet'
+        config['connection']['id'] = self.name
+        config['connection']['uuid'] = self.uuid
+        config['connection']['permissions'] = ''
+        config['ipv4'] = {}
+        config['ipv4']['may-fail'] = 'false'
+        if self.use_dhcp:
+            config['ipv4']['method'] = 'auto'
+        else:
+            config['ipv4']['method'] = 'manual'
+            config['ipv4']['address1'] = '%s/%s,%s' % (self.ip4_address, self.ip4_gateway, self.ip4_gateway)
+            config['ipv4']['dns'] = ''
+            for i in (self.ip4_dns1, self.ip4_dns2):
+                if i:
+                    config['ipv4']['dns'] += '%s;' %(i,)
+        return config
+
+    def writeFile(self):
+        l = open(self.fileName, 'w')
+        config = self.generateConfig()
+        for i in config:
+            l.write('[%s]\n' % (i,))
+            for j in config[i]:
+                l.write('%s=%s\n' % (j, config[i][j]))
+            l.write('\n')
+        l.close()
+
+    def generateFileName(self):
+        nm_dir = settings.NM_CONNETIONS_DIR
+        files = os.listdir(nm_dir)
+        base_name = ''.join(filter(lambda x: x in string.lowercase+string.uppercase+string.digits,
+                                   self.name))
+        base = os.path.join(nm_dir, base_name)
+        suffix = 0
+        name = base
+        while name in files:
+            name = base + str(suffix)
+            suffix += 1
+        return name
+
+    def save(self, *args, **kwargs):
+        if not self.uuid:
+            self.uuid = uuid.uuid4()
+        self.writeFile()
+        super(Network, self).save(*args, **kwargs)
 
 class WireslessNetwork(Network):
     SECURITY_TYPE = (
@@ -32,6 +89,19 @@ class WireslessNetwork(Network):
       ('WEP', 'WEP'),
       ('WPA', 'WPA')
     )
+
+    def generateConfig(self):
+        config = super(WireslessNetwork, self).generateConfig()
+        config['connection']['type'] = 'wifi'
+        config['wifi'] = {}
+        config['wifi']['mode'] = 'infrastructure'
+        config['wifi']['ssid'] = self.essid
+        if self.security_type != 'OP':
+          config['wifi-security'] = {}
+          config['wifi-security']['key-mgmt'] = 'wpa-psk'
+          config['wifi-security']['psk'] = self.password
+        return config
+
     essid = models.CharField(max_length=128)
     security_type = models.CharField(max_length = 8, choices = SECURITY_TYPE)
-    password = models.CharField(max_length=128)
+    password = models.CharField(max_length=128, blank=True)
--- streamPi/streamPi/settings.py
+++ streamPi/streamPi/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+NM_CONNETIONS_DIR = '/etc/NetworkManager/system-connections/'
+NM_SET_ROOT_PERMISSIONS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
